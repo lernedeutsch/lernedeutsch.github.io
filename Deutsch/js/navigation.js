d757c0ebce8bf25c75a8cb84d5c03a62ca569506
deutsch.lernen <moni.s.w@o2.pl>
2026-06-20 17:41:32 -0700

    Update navigation.js

--- Deutsch/js/navigation.js
+++ Deutsch/js/navigation.js
@@ -1,1 +1,19 @@
+const menu = {
+  lessons: [
+    { name: "A1", link: "lessons/A1.html" },
+    { name: "A2", link: "lessons/A2.html" },
+    { name: "B1", link: "lessons/B1.html" }
+  ]
+};
 
+function renderMenu(containerId, section) {
+  const container = document.getElementById(containerId);
+
+  menu[section].forEach(item => {
+    const a = document.createElement("a");
+    a.href = item.link;
+    a.textContent = item.name;
+    a.className = "menu-btn";
+    container.appendChild(a);
+  });
+}
