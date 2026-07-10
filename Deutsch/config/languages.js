d4979a49c7fb1bfca9a903c4076be0eb1e348e26
deutsch.lernen <moni.s.w@o2.pl>
2026-07-10 16:12:02 -0700

    Update languages.js

--- Deutsch/config/languages.js
+++ Deutsch/config/languages.js
@@ -1,1 +1,34 @@
+/**
+ * languages.js - Obsługa języków (tylko niemiecki)
+ */
 
+const languages = {
+  domyślny: "de",  // ← Zmieniono na "de"
+  wspierane: ["de"],  // ← Tylko niemiecki
+
+  // Tłumaczenia (tylko niemiecki)
+  tłumaczenia: {
+    de: {
+      nazwa: "Lerne Deutsch",
+      opis: "Deutsch lernen",
+      lekcje: "Lektionen",
+      ćwiczenia: "Übungen",  // ← Zmieniono na "Übungen"
+      chatbot: "Chatbot",
+      gramatyka: "Grammatik",
+      słownictwo: "Wortschatz",  // ← Zmieniono na "Wortschatz"
+      start: "Starten",
+      mapa: "Karte",
+     タワー: "Turm"  // ← Usunąć, jeśli niepotrzebne
+    }
+  }
+};
+
+// Funkcja pomocnicza do pobierania tłumaczenia
+function getTranslation(klucz) {
+  return languages.tłumaczenia[languages.domyślny][klucz] || "";
+}
+
+// Eksport
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = languages;
+}
