06e36ce7bc33dbd108b35b5c6a6315bae89b363f
deutsch.lernen <moni.s.w@o2.pl>
2026-07-11 15:37:04 -0700

    Update metadata.js

--- Deutsch/data/A1/metadata.js
+++ Deutsch/data/A1/metadata.js
@@ -1,1 +1,15 @@
+/**
+ * metadata.js - Metadane poziomu A1
+ */
 
+const metadataA1 = {
+  level: "A1",
+  lessonsCount: 3,
+  author: "Lerne Deutsch",
+  version: "0.1.0",
+  topics: ["Begrüßung", "Sich vorstellen", "Herkunft und Wohnort"]
+};
+
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = metadataA1;
+}
