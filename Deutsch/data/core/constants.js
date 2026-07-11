b5908a0a59ac24f45f49d3698c42c6a9a539943b
deutsch.lernen <moni.s.w@o2.pl>
2026-07-11 15:29:50 -0700

    Update constants.js

--- Deutsch/data/core/constants.js
+++ Deutsch/data/core/constants.js
@@ -1,1 +1,15 @@
+/**
+ * constants.js - Stałe aplikacji
+ */
 
+const LEVELS = ["A1", "A2", "B1", "B2", "C1", "C2"];
+
+const SKILLS = ["Hören", "Lesen", "Schreiben", "Sprechen"];
+
+const APP_NAME = "Lerne Deutsch";
+
+const DEFAULT_LEVEL = "A1";
+
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = { LEVELS, SKILLS, APP_NAME, DEFAULT_LEVEL };
+}
