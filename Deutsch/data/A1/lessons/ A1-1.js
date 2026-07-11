7627264d2daa1b76c7c0689d3545eb9f5a7d84dd
deutsch.lernen <moni.s.w@o2.pl>
2026-07-11 15:33:59 -0700

    Add lesson A1-1 for basic German greetings

--- Deutsch/data/A1/lessons/ A1-1.js
+++ Deutsch/data/A1/lessons/ A1-1.js
@@ -1,1 +1,21 @@
+/**
+ * A1-1.js - Dane lekcji A1-1 (Begrüßung)
+ */
 
+const lessonA11 = {
+  id: "A1-1",
+  title: "Begrüßung",
+  description: "Einfache Begrüßungen auf Deutsch.",
+  examples: [
+    "Hallo!",
+    "Guten Tag!",
+    "Guten Morgen!",
+    "Guten Abend!"
+  ],
+  task: "Schreibe einen Begrüßungssatz, z.B.: „Hallo, ich bin Nele.“",
+  expectedAnswer: "Ich bin Nele."
+};
+
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = lessonA11;
+}
