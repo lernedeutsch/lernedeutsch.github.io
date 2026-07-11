e1548d09d981041d1008b112bf228e466f53f533
deutsch.lernen <moni.s.w@o2.pl>
2026-07-11 15:36:33 -0700

    Add A1 level writing exercises
    Added exercises for A1 level including writing tasks.

--- Deutsch/data/A1/exercises/A1.js
+++ Deutsch/data/A1/exercises/A1.js
@@ -1,1 +1,24 @@
+/**
+ * A1.js - Ćwiczenia dla poziomu A1
+ */
 
+const exercisesA1 = [
+  {
+    id: "a1-ex-1",
+    lessonId: "A1-1",
+    type: "writing",
+    instruction: "Schreibe einen Begrüßungssatz.",
+    example: "Hallo, ich bin Nele."
+  },
+  {
+    id: "a1-ex-2",
+    lessonId: "A1-2",
+    type: "writing",
+    instruction: "Schreibe drei Sätze über dich.",
+    example: "Ich heiße Nele. Ich bin Lehrerin. Ich komme aus Polen."
+  }
+];
+
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = exercisesA1;
+}
