d66d4506ceb44a380502e7bab0d620a5c9ec94dd
deutsch.lernen <moni.s.w@o2.pl>
2026-07-11 15:36:02 -0700

    Update A1.js

--- Deutsch/data/A1/grammar/A1.js
+++ Deutsch/data/A1/grammar/A1.js
@@ -1,1 +1,20 @@
+/**
+ * A1.js - Gramatyka dla poziomu A1
+ */
 
+const grammarA1 = [
+  {
+    id: "a1-satzstellung",
+    title: "Satzstellung",
+    rule: "Subjekt – Verb – Rest.",
+    examples: [
+      "Ich bin Nele.",
+      "Ich komme aus Polen.",
+      "Ich wohne in Frankfurt."
+    ]
+  }
+];
+
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = grammarA1;
+}
